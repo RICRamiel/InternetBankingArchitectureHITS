85e6c7318ecd8305f18f93a8a0ab4b96bf6b3849
Vasilii Polushkin <kvvarta@yandex.ru>
2026-02-27 00:32:00 +0700

    pom fix

--- core-api/src/main/java/org/ricramiel/coreapi/kafka/producer/TransactionOperationProducer.java
+++ core-api/src/main/java/org/ricramiel/coreapi/kafka/producer/TransactionOperationProducer.java
@@ -1,11 +1,12 @@
 package org.ricramiel.coreapi.kafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.ricramiel.coreapi.entity.TransactionOperation;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
@@ -16,7 +17,7 @@ public class TransactionOperationProducer {
     private final ObjectMapper objectMapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void sendTransactionOperation(TransactionOperation transactionOperation) {
+    public void sendTransactionOperation(TransactionOperation transactionOperation) throws JsonProcessingException {
         kafkaTemplate.send(topic, objectMapper.writeValueAsString(transactionOperation));
     }
 }
