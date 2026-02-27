f57914fa3d82c531d7979c9001932b747c9afdeb
Vasilii Polushkin <kvvarta@yandex.ru>
2026-02-27 21:40:43 +0700

    mapper

---
+++ core-api/src/main/java/org/ricramiel/coreapi/mapper/PaginationMapper.java
@@ -0,0 +1,16 @@
+package org.ricramiel.coreapi.mapper;
+
+import org.ricramiel.common.dtos.PaginationDto;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaginationMapper {
+    public <T> PaginationDto toDto(Page<T> domainPage) {
+        return new PaginationDto(
+                domainPage.getNumber(),
+                domainPage.getSize(),
+                domainPage.getTotalPages()
+        );
+    }
+}
