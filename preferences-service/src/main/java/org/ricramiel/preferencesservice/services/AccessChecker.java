d059101f478b50529b5f631143e96bce717cdae9
Vasilii Polushkin <kvvarta@yandex.ru>
2026-03-21 22:27:08 +0700

    access checkers

---
+++ preferences-service/src/main/java/org/ricramiel/preferencesservice/services/AccessChecker.java
@@ -0,0 +1,18 @@
+package org.ricramiel.preferencesservice.services;
+
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@AllArgsConstructor
+public class AccessChecker {
+    private final CurrentUserService userService;
+
+    public boolean isSelf(@NonNull UUID userId) {
+        UUID currentUserId = userService.getUserId();
+        return userId.equals(currentUserId);
+    }
+}
---
+++ transaction-service/src/main/java/org/ricramiel/transactionservice/service/CurrentUserService.java
@@ -0,0 +1,29 @@
+package org.ricramiel.transactionservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.ricramiel.common.exceptions.status_code_exceptions.UnauthorizedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CurrentUserService {
+
+    public UUID getUserId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication.getPrincipal() instanceof String id) {
+            try {
+                return UUID.fromString(id);
+            }
+            catch (Exception e) {
+                throw new UnauthorizedException("Invalid user id");
+            }
+        }
+
+        throw new IllegalStateException("Invalid authentication type");
+    }
+}
