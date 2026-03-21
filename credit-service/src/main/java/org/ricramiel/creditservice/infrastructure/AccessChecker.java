fb8747c62e32d049d23ee31e5f96f1754e376cee
Vasilii Polushkin <kvvarta@yandex.ru>
2026-03-21 22:35:10 +0700

    access checkers

--- credit-service/src/main/java/org/ricramiel/creditservice/infrastructure/AccessChecker.java
+++ credit-service/src/main/java/org/ricramiel/creditservice/infrastructure/AccessChecker.java
@@ -1,7 +1,10 @@
 package org.ricramiel.creditservice.infrastructure;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import org.ricramiel.creditservice.model.Credit;
+import org.ricramiel.creditservice.repository.CreditRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -10,9 +13,17 @@ import java.util.UUID;
 @AllArgsConstructor
 public class AccessChecker {
     private final CurrentUserService userService;
+    private final CreditRepository creditRepository;
 
     public boolean isSelf(@NonNull UUID userId) {
         UUID currentUserId = userService.getUserId();
         return userId.equals(currentUserId);
+    }
+
+    public boolean isOwner(UUID creditId) {
+        Credit credit = creditRepository.findById(creditId)
+                .orElseThrow(() -> new EntityNotFoundException("Credit not found with id: " + creditId));
+
+        return credit.getUserId().equals(userService.getUserId());
     }
 }
