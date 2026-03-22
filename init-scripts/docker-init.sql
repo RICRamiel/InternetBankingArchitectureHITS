a541b8c445cbff1a523207ad0f4228088d767cdb
Ramiel <tardis40type@gmail.com>
2026-03-22 23:29:23 +0700

    docker-update

--- init-scripts/docker-init.sql
+++ init-scripts/docker-init.sql
@@ -10,6 +10,7 @@ GRANT ALL PRIVILEGES ON DATABASE userservice TO c417110f943e48c5a62d45633bdf9ea2
 GRANT ALL PRIVILEGES ON DATABASE transactionservice TO c417110f943e48c5a62d45633bdf9ea2;
 GRANT ALL PRIVILEGES ON DATABASE preferencesservice TO c417110f943e48c5a62d45633bdf9ea2;
 
+-- Не рабочее решение т.к таблички в которые они заносятся создаются на этапе включения приложений, т.е их там нет что приводит к ошибкам
 INSERT INTO public.card_account (id,currency,deleted,is_main,money,name,user_id) VALUES ('18f35126-66c0-4997-9922-39c70e924634'::uuid,'RUB',false,false,100000.00,'MASTER_CARD_ACCOUNT','b4d01963-ac7a-4644-923f-1cb66a17c157'::uuid);
 
 INSERT INTO public.users (id,email,is_active,name,"password") VALUES
